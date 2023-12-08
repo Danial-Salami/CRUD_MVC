78555cc2ee1f85ddaf241fac28b63322e3c2384a
Danial0021 <Danial0021@DESKTOP-1VR01KU>
2023-12-08 21:09:50 +0330

    Add project files.

---
+++ Tickets-Booking/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tickets_Booking.Models;
+
+namespace Tickets_Booking.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
